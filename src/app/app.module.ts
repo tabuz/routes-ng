f3a37611946f42a8ae1ad87d5a9ba8f0a9bb517d
tabuz <zelek.hubert@gmail.com>
2017-01-31 22:32:33 +0000

    routes update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { APP_ROUTES_PROVIDER } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from "./user/user.component";
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from "@angular/router";
+
+import { UserComponent } from "./user/user.component";
+import { HomeComponent } from "./home-component.component";
+const APP_ROUTES: Routes = [
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  }
+];
+export const APP_ROUTES_PROVIDER = RouterModule.forRoot(APP_ROUTES);
